ce3c76716d41e951a477e766d26185d8860377b0
Marton Paulo <martonpss@gmail.com>
2019-10-24 21:18:13 -0300

    Update testes-triangulo.js

--- src/test/testes-triangulo.js
+++ src/test/testes-triangulo.js
@@ -5,85 +5,116 @@ const assert = chai.assert;
 
 
 describe('Verificando se é escaleno', () => {
-  it('Teste: Os três lados são diferentes', () => {
+
+  it('\n\tTeste: Os três lados são diferentes', () => {
     let triangulo = new Triangulo(7, 5, 10);
-    assert.equal(triangulo.tipo(), "Escaleno");
+    if(triangulo.valido()) assert.equal(triangulo.tipo(), "Escaleno");
   });
 
 });
+
 
 describe('Verificando se é equilatero', () => {
-  it('Teste: Os três lados são iguais', () => {
+
+  it('\n\tTeste: Os três lados são iguais', () => {
     let triangulo = new Triangulo(5, 5, 5);
-    assert.equal(triangulo.tipo(), "Equilatero");
+    if(triangulo.valido()) assert.equal(triangulo.tipo(), "Equilatero");
   });
+
 });
+
 
 describe('Verificando se é isósceles', () => {
-  it('Teste: Lados a e b iguais', () => {
+
+  it('\n\tTeste: Lados a e b iguais', () => {
     let triangulo = new Triangulo(7, 7, 10);
-    assert.equal(triangulo.tipo(), "Isosceles");
+    if(triangulo.valido()) assert.equal(triangulo.tipo(), "Isosceles");
   });
-  it('Teste: Lados b e c iguais', () => {
+
+  it('\n\tTeste: Lados b e c iguais', () => {
     let triangulo = new Triangulo(10, 7, 7);
-    assert.equal(triangulo.tipo(), "Isosceles");
+    if(triangulo.valido()) assert.equal(triangulo.tipo(), "Isosceles");
   });
-  it('Teste: Lados a e c iguais', () => {
+
+  it('\n\tTeste: Lados a e c iguais', () => {
     let triangulo = new Triangulo(7, 10, 7);
-    assert.equal(triangulo.tipo(), "Isosceles");
+    if(triangulo.valido()) assert.equal(triangulo.tipo(), "Isosceles");
   });
+
 });
 
+
 describe('Verificando se os tamanhos lados são válidos', () => {
-  it('Teste: Lado com valor que viola validade do triângulo', () => {
+
+  it('\n\tTeste: Lado com valor que viola validade do triângulo', () => {
     let triangulo = new Triangulo(100, 10, 10);
     assert.equal(triangulo.valido(), false);
   });
-  it('Teste: Lado com valor negativo', () =>{
+  
+  it('\n\tTeste: Lado com valor negativo', () => {
     let triangulo = new Triangulo(-6, 8, 10);
     assert.equal(triangulo.valido(), false);
   });
-  it('Teste: Lados com valores válidos', () =>{
+
+  it('\n\tTeste: Lado com valor nulo', () => {
+    let triangulo = new Triangulo(0, 8, 10);
+    assert.equal(triangulo.valido(), false);
+  });
+  
+  it('\n\tTeste: Lados com valores válidos', () => {
     let triangulo = new Triangulo(25, 24, 7);
     assert.equal(triangulo.valido(), true);
   });
-  it('Teste: Parâmetros como string', () => {
-    let triangulo = new Triangulo('10','c', 8);
+  
+  // o programa deve converter string para números quando possível
+  it('\n\tTeste: Parâmetros como string de números', () => {
+    let triangulo = new Triangulo('3','4', 5);
+    assert.equal(triangulo.valido(), true);
+  });
+
+  it('\n\tTeste: Parâmetros como string de carcteres não-numéricos', () => {
+    let triangulo = new Triangulo(3, 4, 'c');
     assert.equal(triangulo.valido(), false);
   });
+
 });
 
 
 describe('Verificando se a área calculada é coerente', () => {
-  it('Teste: Área incorreta', () => {
+  
+  it('\n\tTeste: Área incorreta', () => {
     let triangulo = new Triangulo(6, 8, 10);
-    assert.equal(triangulo.area(), 24);
+    if(triangulo.valido()) assert.equal(triangulo.area(), 24);
   });
-});
 
+});
 
 
 describe('Verificando se é retângulo', () => {
-  it('Teste: Existe um ângulo reto', () => {
+  
+  it('\n\tTeste: Existe um ângulo reto', () => {
     let triangulo = new Triangulo(3, 4, 5);
-    assert.equal(triangulo.tipoAngulo(), 'Retangulo');
+    if(triangulo.valido()) assert.equal(triangulo.tipoAngulo(), 'Retangulo');
   });
+
 });
+
 
 describe('Verificando se é acutângulo', () => {
-  it('Teste: Os três ângulos são agudos', () => {
+  
+  it('\n\tTeste: Os três ângulos são agudos', () => {
     let triangulo = new Triangulo(2, 4, 5);
-    if(triangulo.valido()){
-      assert.equal(triangulo.tipoAngulo(), 'Obtusangulo');
-    }
+    if(triangulo.valido()) assert.equal(triangulo.tipoAngulo(), 'Obtusangulo');
   });
+
 });
 
+
 describe('Verificando se é obtusângulo', () => {
-  it('Teste: Existe um ângulo obtuso', () => {
+  
+  it('\n\tTeste: Existe um ângulo obtuso', () => {
     let triangulo = new Triangulo(4, 4, 5);
-    if(triangulo.valido()){
-      assert.equal(triangulo.tipoAngulo(), 'Acutangulo');
-    }
+    if(triangulo.valido()) assert.equal(triangulo.tipoAngulo(), 'Acutangulo');
   });
+
 });
