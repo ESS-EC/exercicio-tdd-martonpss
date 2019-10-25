1b444aeeff53cc9db75f24902de2cd704058b5bb
Marton Paulo <martonpss@gmail.com>
2019-10-24 21:17:44 -0300

    Update triangulo.js

--- src/triangulo.js
+++ src/triangulo.js
@@ -1,37 +1,38 @@
-function Triangulo(a,b,c) {
+function Triangulo(a, b, c) {
   this.a = a;
   this.b = b;
   this.c = c;
-
 }
 
 
 
 
 Triangulo.prototype.toString = function () {
-  return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`
+  return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`;
 };
 
 
-Triangulo.prototype.tipo = function(){
-  if(this.a === this.b && this.b === this.c){
-    return 'Equilatero';
-
-  }else if(this.a === this.b || this.b === this.c || this.a === this.c){
-    return 'Isosceles';
-  
-  }else{
-    return 'Escaleno';
-  }
-}
-
-
 Triangulo.prototype.valido = function(){
-  
-  if(typeof(this.a) !== 'number' || typeof(this.b) !== 'number' || typeof(this.c) !== 'number'){
-    return false;
 
-  }else if((this.a <= 0) || (this.b <= 0) || (this.c <= 0)){
+  if(typeof(this.a) !== 'number'){
+    // converte 'a' para número se 'a' for uma string com números
+    this.a = Number(this.a);
+    if(isNaN(this.a)) return false;
+  }
+
+  if(typeof(this.b) !== 'number'){
+    this.b = Number(this.b);
+    if(isNaN(this.b)) return false;
+  }
+
+  if(typeof(this.c) !== 'number'){
+    this.c = Number(this.c);
+    if(isNaN(this.c)) return false;
+  }
+
+
+
+  if((this.a <= 0) || (this.b <= 0) || (this.c <= 0)){
     return false;
   
   }else if( (this.a < (this.b + this.c)) && (this.b < (this.a + this.c)) && (this.c < (this.b + this.a))) {
@@ -42,6 +43,19 @@ Triangulo.prototype.valido = function(){
   }
 }
 
+
+Triangulo.prototype.tipo = function(){
+
+  if(this.a === this.b && this.b === this.c){
+    return 'Equilatero';
+
+  }else if(this.a === this.b || this.b === this.c || this.a === this.c){
+    return 'Isosceles';
+  
+  }else{
+    return 'Escaleno';
+  }
+}
 
 Triangulo.prototype.area = function(){
 
